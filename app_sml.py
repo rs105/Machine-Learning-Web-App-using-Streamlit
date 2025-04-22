84fff6a54cf1ffdcf267f9a1b89ef0fe0f781888
Romit Sen <senromit7@gmail.com>
2025-04-22 17:20:16 -0500

    Added .py file and the .csv file

---
+++ app_sml.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 22 03:06:23 2025
+
+@author: senro
+"""
+
+import os
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, roc_curve, precision_recall_curve, auc, ConfusionMatrixDisplay, RocCurveDisplay, PrecisionRecallDisplay
+from sklearn.metrics import precision_score, recall_score
+
+def main():
+    st.title("Binary Classification Web App")
+    st.sidebar.title("Binary Classification Web App")
+    st.markdown("Are your mushrooms edible or poisonous? 🍄")
+    st.sidebar.markdown("Are your mushrooms edible or poisonous? 🍄")
+
+    @st.cache_data(persist=True) # Use the cache output from the disk whenever app reruns - time efficiency increases
+    # Function for loading data and using label encoder to encode the categorical columns
+    def load_data():
+        data = pd.read_csv("C:\\Users\\senro\\Downloads\\OSU - Spring 2025\\Streamlit-ML\\Streamlit-ML-code\\mushrooms.csv")
+        labelencoder=LabelEncoder()
+        for col in data.columns:
+            data[col] = labelencoder.fit_transform(data[col])
+        return data
+    
+    @st.cache_data(persist=True)
+    # Function for creating the train, test dataset for training and evaluating the model accuracy
+    def split(df):
+        y = df.type
+        x = df.drop(columns=['type'])
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
+        return x_train, x_test, y_train, y_test
+    
+    # Function for plotting evaluation metrics - Confusion Matrix, ROC-AUC curve, Precision-Recall Curves
+    def plot_metrics(metrics_list):
+        if 'Confusion Matrix' in metrics_list:
+            st.subheader("Confusion Matrix")
+            y_pred = model.predict(x_test)
+            cm = confusion_matrix(y_test, y_pred)
+            cm_display = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=class_names)
+            cm_display.plot()
+            st.pyplot()
+    
+        if 'ROC Curve' in metrics_list:
+            st.subheader("ROC Curve")
+            y_pred_prob = model.predict_proba(x_test)[:, 1]  # For binary classification, we use the probability of the positive class
+            fpr, tpr, _ = roc_curve(y_test, y_pred_prob)
+            roc_auc = auc(fpr, tpr) # Calculate AUC
+            roc_display = RocCurveDisplay(fpr=fpr, tpr=tpr)
+            roc_display.plot()
+            
+            # Add AUC label to the bottom-right corner
+            plt.text(0.8, 0.2, f"AUC = {roc_auc:.2f}", horizontalalignment='center', verticalalignment='center', transform=plt.gca().transAxes)
+            st.pyplot()
+    
+        if 'Precision-Recall Curve' in metrics_list:
+            st.subheader('Precision-Recall Curve')
+            y_pred_prob = model.predict_proba(x_test)[:, 1]  # For binary classification, uwe se the probability of the positive class
+            precision, recall, _ = precision_recall_curve(y_test, y_pred_prob)
+            pr_auc = auc(recall, precision) # Calculate AUC for Precision-Recall Curve
+            pr_display = PrecisionRecallDisplay(precision=precision, recall=recall)
+            pr_display.plot()
+            
+            # Add Precision-Recall Curve AUC label to the bottom-right corner
+            plt.text(0.8, 0.2, f"AUC = {pr_auc:.2f}", horizontalalignment='center', verticalalignment='center', transform=plt.gca().transAxes)
+            st.pyplot()
+
+    df = load_data()
+    class_names = ['edible', 'poisonous']
+    
+    x_train, x_test, y_train, y_test = split(df)
+
+    st.sidebar.subheader("Choose Classifier")
+    classifier = st.sidebar.selectbox("Classifier", ("Support Vector Machine (SVM)", "Logistic Regression", "Random Forest"))
+
+    if classifier == 'Support Vector Machine (SVM)':
+        st.sidebar.subheader("Model Hyperparameters")
+        C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key='C_SVM')
+        kernel = st.sidebar.radio("Kernel", ("rbf", "linear"), key='kernel')
+        gamma = st.sidebar.radio("Gamma (Kernel Coefficient)", ("scale", "auto"), key='gamma')
+
+        metrics = st.sidebar.multiselect("What metrics to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
+        
+        if st.sidebar.button("Classify", key='classify'): # Ensure that the webpage reloads and displays the new metrics, prevent auto reload on each metric change
+            st.subheader("Support Vector Machine (SVM) Results")
+            model = SVC(C=C, kernel=kernel, gamma=gamma, probability=True)
+            model.fit(x_train, y_train)
+            accuracy = model.score(x_test, y_test)
+            y_pred = model.predict(x_test)
+            st.write("Accuracy: ", round(accuracy,2))
+            st.write("Precision: ", round(precision_score(y_test, y_pred, labels=class_names),2))
+            st.write("Recall: ", round(recall_score(y_test, y_pred, labels=class_names),2))
+            plot_metrics(metrics)
+    
+    if classifier == 'Logistic Regression':
+        st.sidebar.subheader("Model Hyperparameters")
+        C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key='C_LR')
+        max_iter = st.sidebar.slider("Maximum number of iterations", 100, 500, key='max_iter')
+
+        metrics = st.sidebar.multiselect("What metrics to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
+
+        if st.sidebar.button("Classify", key='classify'):
+            st.subheader("Logistic Regression Results")
+            model = LogisticRegression(C=C, penalty='l2', max_iter=max_iter)
+            model.fit(x_train, y_train)
+            accuracy = model.score(x_test, y_test)
+            y_pred = model.predict(x_test)
+            st.write("Accuracy: ", round(accuracy,2))
+            st.write("Precision: ", round(precision_score(y_test, y_pred, labels=class_names),2))
+            st.write("Recall: ", round(recall_score(y_test, y_pred, labels=class_names),2))
+            plot_metrics(metrics)
+    
+    if classifier == 'Random Forest':
+        st.sidebar.subheader("Model Hyperparameters")
+        n_estimators = st.sidebar.number_input("The number of trees in the forest", 100, 5000, step=10, key='n_estimators')
+        max_depth = st.sidebar.number_input("The maximum depth of the tree", 1, 20, step=1, key='max_depth')
+        bootstrap = st.sidebar.radio("Bootstrap samples when building trees", ('True', 'False'), key='bootstrap')
+        bootstrap = bootstrap == 'True' # Converting the string values - 'True', 'False' returned from radio to boolean True, False
+        metrics = st.sidebar.multiselect("What metrics to plot?", ('Confusion Matrix', 'ROC Curve', 'Precision-Recall Curve'))
+
+        if st.sidebar.button("Classify", key='classify'):
+            st.subheader("Random Forest Results")
+            model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, bootstrap=bootstrap, n_jobs=-1) # n_jobs = -1 implies that using all cores of the CPU for computation
+            model.fit(x_train, y_train)
+            accuracy = model.score(x_test, y_test)
+            y_pred = model.predict(x_test)
+            st.write("Accuracy: ", round(accuracy,2))
+            st.write("Precision: ", round(precision_score(y_test, y_pred, labels=class_names),2))
+            st.write("Recall: ", round(recall_score(y_test, y_pred, labels=class_names),2))
+            plot_metrics(metrics)
+
+    if st.sidebar.checkbox("Show raw data", False):
+        st.subheader("Mushroom Data Set (Classification)")
+        st.write(df)
+        st.markdown("This [data set](https://archive.ics.uci.edu/ml/datasets/Mushroom) includes descriptions of hypothetical samples corresponding to 23 species of gilled mushrooms "
+        "in the Agaricus and Lepiota Family (pp. 500-525). Each species is identified as definitely edible, definitely poisonous, "
+        "or of unknown edibility and not recommended. This latter class was combined with the poisonous one.")
+
+if __name__ == '__main__':
+    os.system('streamlit run app_sml.py')
+    main()
+
+
